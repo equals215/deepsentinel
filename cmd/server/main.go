2d656a4796c4ef8c27a5753c479b2e08ba3cf423
Thomas FOUBERT <perso@thomas-foubert.com>
2024-03-16 19:34:40 -0400

    basecmd + go mod

---
+++ cmd/server/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	rootCmd := &cobra.Command{
+		Use:   "deepsentinel-server",
+		Short: "deepSentinel server CLI",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Help()
+		},
+	}
+	rootCmd.CompletionOptions.DisableDefaultCmd = true
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
