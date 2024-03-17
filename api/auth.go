5599c2ec751e93dae5fcf012a04afad37724823b
Thomas FOUBERT <perso@thomas-foubert.com>
2024-03-17 16:35:49 -0400

    feat: make healthcheck endpoint rfc-healthcheck compliant + boilerplate reporting endpoint

--- api/auth.go
+++ api/auth.go
@@ -3,11 +3,28 @@ package api
 import (
 	"crypto/sha256"
 	"crypto/subtle"
+	"regexp"
+	"strings"
 
 	"github.com/equals215/deepsentinel/config"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/keyauth"
 )
+
+var protectedURLs = []*regexp.Regexp{
+	regexp.MustCompile("^/report$"),
+}
+
+func authFilter(c *fiber.Ctx) bool {
+	originalURL := strings.ToLower(c.OriginalURL())
+
+	for _, pattern := range protectedURLs {
+		if pattern.MatchString(originalURL) {
+			return false
+		}
+	}
+	return true
+}
 
 func validateAuth(_ *fiber.Ctx, givenKey string) (bool, error) {
 	hashedKey := sha256.Sum256([]byte(config.Server.AuthToken))
--- api/server.go
+++ api/server.go
@@ -9,12 +9,23 @@ func newServer() *fiber.App {
 	app := fiber.New()
 
 	app.Use(keyauth.New(keyauth.Config{
+		Next:      authFilter,
 		KeyLookup: "header:Authorization",
 		Validator: validateAuth,
 	}))
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋")
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"status": "pass",
+		})
+	})
+
+	app.Post("/report/:machine", func(c *fiber.Ctx) error {
+		machine := c.Params("machine")
+		return c.JSON(fiber.Map{
+			"status":  "received",
+			"machine": machine,
+		})
 	})
 
 	return app
