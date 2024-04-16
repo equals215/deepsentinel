f1bef2dae242c5049b5dad7dc84f043fd5eafbb1
Thomas FOUBERT <perso@thomas-foubert.com>
2024-04-15 21:34:19 -0400

    feat: added launchd support, daemon uninstall unregister the probe, binary are stored in a different dir

--- agent/unregister.go
+++ agent/unregister.go
@@ -14,7 +14,7 @@ func UnregisterCmd(rootCmd *cobra.Command) {
 		Args:  cobra.ExactArgs(0),
 		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Println("Unregistering agent")
-			doConfigInstruction("unregister", args)
+			DoConfigInstruction("unregister", args)
 		},
 	}
 
---
+++ daemonize/launchd.go
@@ -0,0 +1,89 @@
+package daemonize
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/brasic/launchd"
+	"github.com/brasic/launchd/state"
+	"github.com/equals215/deepsentinel/agent"
+)
+
+var (
+	launchdServerServiceFileName = "com.deepsentinel.server.plist"
+	launchdAgentServiceFileName  = "com.deepsentinel.agent.plist"
+	launchdServerServiceFilePath = "/Library/LaunchDaemons/" + launchdServerServiceFileName
+	launchdAgentServiceFilePath  = "/Library/LaunchDaemons/" + launchdAgentServiceFileName
+)
+
+type launchdDaemon struct {
+	component daemonType
+	service   launchd.Service
+}
+
+func (d *launchdDaemon) primeDaemon() {
+	d.service.Name = fmt.Sprintf("%s-%s", "deepsentinel", d.component.String())
+	d.service.ExecutablePath = d.component.binaryPath()
+	d.service.Argv = []string{"run"}
+	d.service.RunAtLoad = true
+	d.service.KeepAlive = true
+}
+
+func (d *launchdDaemon) isDaemonInstalled() bool {
+	daemonState := d.service.InstallState()
+	return daemonState.Is(state.Installed)
+}
+
+func (d *launchdDaemon) isDaemonRunning() bool {
+	return d.service.IsHealthy()
+}
+
+func (d *launchdDaemon) installDaemon() error {
+	installBinary(d.component)
+	return d.service.Install()
+}
+
+func (d *launchdDaemon) uninstallDaemon() error {
+	removePlist := true
+
+	err := agent.DoConfigInstruction("unregister", nil)
+	if err != nil {
+		return err
+	}
+
+	// Wait for the agent to stop
+	fmt.Println("Waiting for the agent to unregister successfully...")
+	time.Sleep(5 * time.Second)
+
+	return d.service.Bootout(removePlist)
+}
+
+func (d *launchdDaemon) updateDaemon() error {
+	err := d.uninstallDaemon()
+	if err != nil {
+		return err
+	}
+
+	return d.installDaemon()
+}
+
+func (d *launchdDaemon) stopDaemon() error {
+	err := agent.DoConfigInstruction("unregister", nil)
+	if err != nil {
+		return err
+	}
+
+	return d.service.Stop()
+}
+
+func (d *launchdDaemon) startDaemon() error {
+	return d.service.Start()
+}
+
+func (d *launchdDaemon) restartDaemon() error {
+	err := d.stopDaemon()
+	if err != nil {
+		return err
+	}
+	return d.startDaemon()
+}
