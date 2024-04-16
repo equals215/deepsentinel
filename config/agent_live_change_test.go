d2fe4839c2b8e69e6fddfe72b28764fea14fe865
Thomas FOUBERT <perso@thomas-foubert.com>
2024-04-16 15:35:59 -0400

    feat: add config tests

---
+++ config/agent_live_change_test.go
@@ -0,0 +1,79 @@
+package config
+
+import (
+	"testing"
+
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAgentSetServerAddress(t *testing.T) {
+	// Test case 1: Agent is nil
+	Agent = nil
+	err := AgentSetServerAddress("http://localhost:8080")
+	assert.NoError(t, err)
+	assert.Equal(t, "http://localhost:8080", viper.GetString("server-address"))
+
+	// Test case 2: Missing address
+	Agent = &AgentConfig{}
+	err = AgentSetServerAddress()
+	assert.EqualError(t, err, "missing address")
+
+	// Test case 3: Too many arguments
+	err = AgentSetServerAddress("localhost:8080", "extra-arg")
+	assert.EqualError(t, err, "too many arguments")
+
+	// Test case 4: Inalid address
+	err = AgentSetServerAddress("localhost:8080")
+	assert.EqualError(t, err, "URL scheme is required")
+
+	// Test case 5: Agent is nil
+	Agent = nil
+	err = AgentSetServerAddress("http://localhost:8080")
+	assert.NoError(t, err)
+	assert.Equal(t, "http://localhost:8080", viper.GetString("server-address"))
+}
+
+func TestAgentSetAuthToken(t *testing.T) {
+	// Test case 1: Agent is nil
+	Agent = nil
+	err := AgentSetAuthToken("token123")
+	assert.NoError(t, err)
+	assert.Equal(t, "token123", viper.GetString("auth-token"))
+
+	// Test case 2: Missing token
+	Agent = &AgentConfig{}
+	err = AgentSetAuthToken()
+	assert.EqualError(t, err, "missing token")
+
+	// Test case 3: Too many arguments
+	err = AgentSetAuthToken("token123", "extra-arg")
+	assert.EqualError(t, err, "too many arguments")
+
+	// Test case 4: Valid token
+	err = AgentSetAuthToken("token123")
+	assert.NoError(t, err)
+	assert.Equal(t, "token123", viper.GetString("auth-token"))
+}
+
+func TestAgentSetMachineName(t *testing.T) {
+	// Test case 1: Agent is nil
+	Agent = nil
+	err := AgentSetMachineName("machine1")
+	assert.NoError(t, err)
+	assert.Equal(t, "machine1", viper.GetString("machine-name"))
+
+	// Test case 2: Missing machine name
+	Agent = &AgentConfig{}
+	err = AgentSetMachineName()
+	assert.EqualError(t, err, "missing machine name")
+
+	// Test case 3: Too many arguments
+	err = AgentSetMachineName("machine1", "extra-arg")
+	assert.EqualError(t, err, "too many arguments")
+
+	// Test case 4: Valid machine name
+	err = AgentSetMachineName("machine1")
+	assert.NoError(t, err)
+	assert.Equal(t, "machine1", viper.GetString("machine-name"))
+}
