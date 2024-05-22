a3b36be527a4dc6d1722a666a6f8a6dcf304d2ac
Thomas FOUBERT <perso@thomas-foubert.com>
2024-05-22 18:09:15 -0400

    feat: add install command for agent that prompts the user for the config parameters

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/equals215/deepsentinel/agent"
+	"github.com/equals215/deepsentinel/config"
 	"github.com/equals215/deepsentinel/daemonize"
 	"github.com/spf13/cobra"
 )
@@ -23,9 +25,68 @@ func main() {
 	agent.Cmd(rootCmd)
 	agent.ConfigCmd(rootCmd)
 	agent.UnregisterCmd(rootCmd)
+	installCmd(rootCmd)
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func installCmd(rootCmd *cobra.Command) {
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "install",
+		Short: "Install the deepSentinel agent",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			var response string
+
+			if os.Geteuid() != 0 {
+				return fmt.Errorf("you must run this command as sudo (preffered) or root")
+			}
+
+			if os.Getenv("SUDO_USER") == "" {
+				fmt.Println("Running as root instead of sudo will force you to run as root in the future to configure the agent")
+				fmt.Print("Do you want to continue? (y/n) ")
+				fmt.Scanln(&response)
+				if response != "y" {
+					return fmt.Errorf("installation aborted")
+				}
+			}
+			return nil
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			var serverAddress string
+			var authToken string
+			var machineName string
+
+			err := os.MkdirAll("/etc/deepsentinel/", 0644)
+			if err != nil {
+				return err
+			}
+
+			fmt.Print("Input the server address (ex.: http://localhost:8080): ")
+			fmt.Scanln(&serverAddress)
+			err = config.AgentSetServerAddress(strings.ReplaceAll(serverAddress, "\n", ""))
+			if err != nil {
+				return err
+			}
+
+			fmt.Print("Input the auth token: ")
+			fmt.Scanln(&authToken)
+			err = config.AgentSetAuthToken(strings.ReplaceAll(authToken, "\n", ""))
+			if err != nil {
+				return err
+			}
+
+			fmt.Print("Input the machine name: ")
+			fmt.Scanln(&machineName)
+			err = config.AgentSetMachineName(strings.ReplaceAll(machineName, "\n", ""))
+			if err != nil {
+				return err
+			}
+
+			daemonize.Daemonize(daemonize.Agent, false)
+			return nil
+		},
+	})
 }
